3d5d1cea921d1ab6e45450222e9e88e758a1310c
Aino Solin <73231276+ainosolin@users.noreply.github.com>
2021-01-25 19:17:29 +0200

    Tehtävä 1

--- viikko3/Viikko3/src/main/java/Mainclass.java
+++ viikko3/Viikko3/src/main/java/Mainclass.java
@@ -15,6 +15,29 @@ public class Mainclass {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
+
+    BottleDispenser bottledisp = new BottleDispenser();
+
+    bottledisp.addMoney(); /* money = 1 */
+    bottledisp.buyBottle(); /* money = 0 */
+
+    /* pullo tulee maatista */
+
+    bottledisp.buyBottle(); /* money = 0, bottle = 4 */
+
+    /* if lause herjaa "Add money first!" */
+
+    bottledisp.addMoney(); /* money = 1 */
+    bottledisp.addMoney(); /* money = 2 */
+    bottledisp.buyBottle(); /* bottle = 3 */
+    
+    /* pullo tulee maatista */
+    
+    bottledisp.returnMoney(); 
+    
+    /* palauta rahat */
+    
     }
     
 }
--- viikko3/Viikko3/src/main/java/bottleDispenser.java
+++ viikko3/Viikko3/src/main/java/bottleDispenser.java
@@ -8,6 +8,38 @@
  *
  * @author asoli
  */
-public class bottleDispenser {
+public class BottleDispenser {
+    private int bottles;
+    private int money;
     
+    public BottleDispenser() {
+        bottles = 5;
+        money = 0;
+    }
+    
+    public void addMoney() {
+        money += 1;
+        System.out.println("Klink! Added more money!");
+    }
+    
+    public void buyBottle() {
+        if (money == 0){
+            money = 0;
+            System.out.println("Add money first!");
+        }
+        else if (bottles < 0){
+            bottles = 0;
+            System.out.println("No bottles left, cannot buy bottles.");
+        }
+        else {
+            bottles -= 1;
+            money -= 1;
+            System.out.println("KACHUNK! Bottle came out of the dispenser!");
+        }
+    }
+    
+    public void returnMoney() {
+        money = 0;
+        System.out.println("Klink klink!! All money gone!");
+    }
 }
