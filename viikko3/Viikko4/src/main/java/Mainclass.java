ccfe572e269168191ad30e859be3f03865105d9e
Aino Solin <73231276+ainosolin@users.noreply.github.com>
2021-02-12 16:11:57 +0200

    Adding Week 5 files

--- viikko3/Viikko4/src/main/java/Mainclass.java
+++ viikko3/Viikko4/src/main/java/Mainclass.java
@@ -29,11 +29,28 @@ public class Mainclass {
         
         String pathName = System.getProperty( "user.dir" );
         Path path = Paths.get(pathName);
+        //System.out.println(path);
         
         ReadAndWriteIO lukija = new ReadAndWriteIO();
         // Tehtävä 1: lukija.ReadFile(path);
         // Tehtävä 2: 
-        lukija.ReadAndWrite("asd.txt", "output.txt");
+        // Tehtävä 3:
+        // Tehtävä 4:
+        // lukija.ReadAndWrite("asd.txt", "output.txt");
+        /* 
+        
+        // Tehtävä 6: ZipInput tehtävä.
+        -   Lue tiedosto zipinput.zip niin, että et pura tiedoston 
+            sisältöä automaattisesti minkään ohjelman (WinRAR, WinZip etc.)
+            avulla. 
+        -   Zip-tiedosto sisältää .txt-muotoisen tiedoston, jossa on dataa. 
+        -   Tulosta tämä data näytölle. 
+        -   Tehtävässä ei tarvitse käyttää enää samoja ehtoja kuin edellisissä
+            tehtävissä.
+        */
+        
+        lukija.Zipper("zipinput.zip", path);
+        
     }
     
 }
