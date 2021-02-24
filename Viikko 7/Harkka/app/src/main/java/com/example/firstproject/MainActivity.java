80184d91e8efd5714b99f990c36702b40e3222ff
Aino Solin <73231276+ainosolin@users.noreply.github.com>
2021-02-24 18:48:15 +0200

    Tehtävät 7.1-7.3. samassa filussa.

--- Viikko 7/Harkka/app/src/main/java/com/example/firstproject/MainActivity.java
+++ Viikko 7/Harkka/app/src/main/java/com/example/firstproject/MainActivity.java
@@ -4,17 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView text;
+    TextView text3;
+    EditText nameInput;
+    String name;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //this.printHello();
+        text = (TextView) findViewById(R.id.textView);
     }
 
     public void printHello (View v){
-        System.out.println("Hello world!");
+
+        System.out.println("Hello World!");
+        text.setText("Hello World!");
+
     }
+
+    public void changeTextbyInput (View s){
+        text3 = (TextView) findViewById(R.id.textView3);
+        nameInput = (EditText) findViewById(R.id.nameInput);
+        name = nameInput.getText().toString();
+        text3.setText(name);
+    }
+
 }
