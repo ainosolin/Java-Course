cb2500c25d8332d18a141bc444a9810b26c8cf0c
Aino Solin <73231276+ainosolin@users.noreply.github.com>
2021-03-01 13:43:06 +0200

    Tehtävä 5

--- Viikko 7/Harkka/app/src/main/java/com/example/firstproject/MainActivity.java
+++ Viikko 7/Harkka/app/src/main/java/com/example/firstproject/MainActivity.java
@@ -8,26 +8,51 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
-
+    /*
     TextView text;
-    //TextView text3;
+    TextView text3;
     TextView text4;
     EditText nameInput;
     EditText updateTextRealTime;
     String name;
+    */
 
+    TextInputEditText inputField;
+    EditText inputFileName;
+
+    String userFilename;
+    String userText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputFileName = (EditText) findViewById(R.id.fileName);
+        inputField = (TextInputEditText) findViewById(R.id.inputField);
+
+        /*
+
         text = (TextView) findViewById(R.id.textView);
         text4 = (TextView) findViewById(R.id.textView4);
         updateTextRealTime = (EditText) findViewById(R.id.updateTextRealTime);
 
-        // Tehtävä 4
+        Tehtävä 4
 
         updateTextRealTime.addTextChangedListener(new TextWatcher() {
             @Override
@@ -45,9 +70,77 @@ public class MainActivity extends AppCompatActivity {
                 text4.setText(updateTextRealTime.getText().toString());
             }
         });
+        */
+
     }
 
-    // Tehtävä 1 ja 2
+    // Tehtävä 5
+
+    public void openFile(View v){
+        userFilename = inputFileName.getText().toString();
+        FileInputStream fis = null;
+
+        try {
+            fis = openFileInput(userFilename);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            String text;
+            while ((text = br.readLine()) != null){
+                sb.append(text).append("\n");
+            }
+
+            inputField.setText(sb.toString());
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null){
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        System.out.println("File Loaded");
+    }
+
+    public void saveFile(View v){
+
+        String userText = inputField.getText().toString();
+        userFilename = inputFileName.getText().toString();
+        FileOutputStream fos = null;
+
+        try {
+            fos = openFileOutput(userFilename, MODE_PRIVATE);
+            fos.write(userText.getBytes());
+            inputField.getText().clear();
+            Toast.makeText(this, "Saved to " + getFilesDir() + "/"
+                            + userFilename, Toast.LENGTH_LONG).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        } finally {
+            if (fos != null){
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        System.out.println("File Saved");
+    }
+
+
+    /*
+
+    Tehtävä 1 ja 2
 
     public void printHello (View v){
 
@@ -56,7 +149,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    /* Tehtävä 3
+    Tehtävä 3
 
     public void changeTextbyInput (View s){
         text3 = (TextView) findViewById(R.id.textView3);
